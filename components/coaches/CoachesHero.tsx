efd059686ac9919ce6ad8e32085b4abd42231b0f
Christian Wang <christianwang39@gmail.com>
2025-08-24 03:14:47 +0200

    small font change

--- components/coaches/CoachesHero.tsx
+++ components/coaches/CoachesHero.tsx
@@ -2,7 +2,7 @@ export default function CoachesHero() {
   return (
     <section className="relative h-screen/4 flex items-center justify-center bg-red-700" style={{height: '25vh'}}>
       <div className="relative z-10 text-center text-white px-4 max-w-4xl mx-auto">
-        <h1 className="text-5xl md:text-7xl font-bold text-white">
+        <h1 className="text-4xl md:text-6xl font-bold text-white">
           Our Coaches
         </h1>
       </div>
